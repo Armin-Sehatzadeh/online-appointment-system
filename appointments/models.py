33d3763548fb5038532dd912a123003414e24cd2
Armin <arminsehatzadeh@gmail.com>
2026-08-11 14:06:47 +0330

    feat: add doctor availability

--- appointments/models.py
+++ appointments/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 class User(AbstractUser):
@@ -44,3 +45,65 @@ class Patient(models.Model):
     def __str__(self):
         return f"Patient: {self.user.username}"
     
+    
+class Availability(models.Model):
+    doctor = models.ForeignKey("Doctor", on_delete=models.CASCADE)
+    
+    class Weekday(models.IntegerChoices):
+        MONDAY = 0, "Monday"
+        TUESDAY = 1, "Tuesday"
+        WEDNESDAY = 2, "Wednesday"
+        THURSDAY = 3, "Thursday"
+        FRIDAY = 4, "Friday"
+        SATURDAY = 5, "Saturday"
+        SUNDAY = 6, "Sunday"
+    
+    start_time = models.TimeField()
+    end_time = models.TimeField()       
+    slot_duration = models.IntegerField() 
+    weekday = models.IntegerField(choices=Weekday.choices)
+    
+    def clean(self):
+        
+        if self.start_time >= self.end_time:
+            raise ValidationError(
+                'Start time must be before end time.'
+            )
+
+        if self.slot_duration <= 0:
+            raise ValidationError(
+                'Slot duration must be greater than zero.'
+            )
+            
+        duration = (
+            self.end_time.hour * 60
+            + self.end_time.minute
+        ) - (
+            self.start_time.hour * 60
+            + self.start_time.minute
+        )
+        
+        if duration % self.slot_duration != 0:
+            raise ValidationError('Slot duration must divide the working time exactly.')
+        
+        
+        existing = Availability.objects.filter(
+            doctor=self.doctor,
+            weekday=self.weekday   
+        )
+        
+        if self.pk:
+            existing = existing.exclude(pk=self.pk)
+            
+        for availability in existing:
+            if (
+                self.start_time < availability.end_time
+                and
+                self.end_time > availability.start_time
+            ):
+                raise ValidationError('This availability overlaps with an existing availability.')
+    
+    
+    def __str__(self):
+        return f"{self.doctor} - {self.Weekday(self.weekday).label} - {self.start_time} to {self.end_time}"
+        
